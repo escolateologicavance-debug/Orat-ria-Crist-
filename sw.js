25ce136bae704d6a0d1a03780770b773ef52ee28
Gilberto Moreira <escolateologicavance@gmail.com>
2025-12-30 07:35:13 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
 const CACHE_NAME = 'oratoria-v1';
 const assets = ['index.html', 'manifest.json', 'logo-192.png', 'logo-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
